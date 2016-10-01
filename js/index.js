69a0616fdf91f2f96b1c2e1e3bd93d739bf32a3a
santosh kumar <santosh kumar>
2016-10-01 23:00:58 +0530

    adding side menu JS

--- js/index.js
+++ js/index.js
@@ -58,5 +58,32 @@ $(document).ready(function() {
       $(that).removeClass("clicked");
     }, logoutPhase1);
   });
+  $(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
   
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
 });
